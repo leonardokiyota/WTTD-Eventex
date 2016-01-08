0c87eaf3887acf5426f631b5b7242939b59df7da
Leonardo Kiyota <leonardo.kiyota@gmail.com>
2016-01-08 18:50:42 -0200

    Refatora Geral

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import messages
 from django.core import mail
 from django.http import HttpResponseRedirect
@@ -20,22 +21,25 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    # Send email
-    body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-    mail.send_mail('Confirmação de Inscrição',
-               body,
-               #'contato@eventex.com.br',
-               #['contato@eventex.com.br', form.cleaned_data['email']])
-               'leonardo.kiyota@gmail.com',
-               ['leonardo.kiyota@gmail.com', form.cleaned_data['email']])
+    _send_mail('Confirmação de Inscrição',
+               # from_ = 'contato@eventex.com.br',
+               settings.DEFAULT_FROM_EMAIL,
+               # to = form.cleaned_data['email']
+               form.cleaned_data['email'],
+               'subscriptions/subscription_email.txt',
+               form.cleaned_data
+               )
 
-    # Success feedback
     messages.success(request, 'Inscrição realizada com Sucesso!')
 
     return HttpResponseRedirect('/inscricao/')
+
 
 def new(request):
     return render(request, 'subscriptions/subscription_form.html',
                   {'form': SubscriptionForm()})
 
 
+def _send_mail(subject, from_, to, template_name, context):
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, from_, [from_, to])
