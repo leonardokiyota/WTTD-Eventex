ae72f6ca824a344a7401824ec489a3c9e05c4d6e
Leonardo Kiyota <leonardo.kiyota@gmail.com>
2016-01-09 19:37:30 -0200

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Leonardo Kiyota',
+            cpf='30449860876',
+            email='leokiyota@gmail.com',
+            phone='11-995959171'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have as auto created_at attr. """
+        self.assertIsInstance(self.obj.created_at, datetime)
